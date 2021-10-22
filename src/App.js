020fe99a21c35d231649fb226b9ec179082d1011
Fran <fmjerez@gmail.com>
2021-10-22 12:30:35 +0200

    Terminado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar";
 import UserList from "./components/UserList";
+import {Provider} from "react-redux"
+import store from "../src/store"
 
 function App() {
   return (
-    <div>
+    <Provider store={store}>
      <Navbar/>
      <UserList/>
-    </div>
+    </Provider>
   );
 }
 
